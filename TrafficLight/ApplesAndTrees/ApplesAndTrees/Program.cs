743376e80b44151d2c62a61a3b2793c60631decb
Armin <58628813+Armin0x@users.noreply.github.com>
2020-03-06 23:14:07 +0100

    Add files via upload

---
+++ TrafficLight/ApplesAndTrees/ApplesAndTrees/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ApplesAndTrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = 8, m = 5, branches = 12;
+            int totalTrees, totalBoxes, boxes;
+            string numTrees;
+            int rest;
+            Console.WriteLine("Please enter the number of trees:");
+
+            numTrees = Console.ReadLine();
+            int.TryParse(numTrees, out totalTrees);
+            rest = (totalTrees * n * branches) % m;
+            totalBoxes = (totalTrees * n * branches) / m;
+            boxes = rest + totalBoxes;
+
+            Console.WriteLine("The total number of branches you will need is:  " + boxes);
+            Console.ReadLine();
+        }
+    }
+}
